ad40398fecbeea22826a134d8ac2118bbd7ee0a1
gaby1994 <gabriel.degez@free.fr>
2019-04-11 11:33:25 +0200

    Add files via upload

--- AbstractFileReader.java
+++ AbstractFileReader.java
@@ -4,22 +4,22 @@ import java.io.*;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/**
+/*/**
  *
  * @author licence
  */
 public abstract class AbstractFileReader implements InterfaceFileReader{
-    protected String fileName;
-    protected ArrayList fileContent;
-    protected File file;
-    protected FileInputStream fis = null;
+    public String fileName;
+    public ArrayList<String> fileContent;
+    public File file;
+    public FileInputStream fis = null;
 	
     public AbstractFileReader(String fileName){
 	this.fileName = fileName;
-	this.fileContent = new ArrayList<>();
+	this.fileContent = new ArrayList<String>();
     }
 	
-    @Override
+    //@Override
     public boolean open() {
 	this.file = new File(fileName);		
 	return (file.exists() && file.canRead() && file.isFile());
@@ -27,22 +27,29 @@ public abstract class AbstractFileReader implements InterfaceFileReader{
     
     
     
-    @Override
+    //@Override
     public void read(){	
         try {
-            boolean eof=false;
-            char c;
-            int n;
+            //boolean eof=false;
+            //int n;
+	    //byte[] bytes;
+	    //int nbElem=0;
+	    String line=" ";
+	    
                       
             file=new File(fileName);
-            fis= new FileInputStream(file);
+            fis=new FileInputStream(file);
             
-            while(!eof){
-                n=fis.read();
-                c=(char)n;
-                fileContent.add(c);
+            /*while(eof!=false){
+	      n=fis.read();
+	      this.fileContent.add((char)n);
                 
-            }
+	      }*/
+	    for(int a=fis.read() ;a != 1 ;a = fis.read()){		
+		char c=(char)a;
+		line=line+c;
+		this.fileContent.add(line);		
+	    }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(AbstractFileReader.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
